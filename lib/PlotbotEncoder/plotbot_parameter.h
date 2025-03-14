58541118fb016bf5e9893bb8ad8dba70a979edcb
matl-hsk <127616095+matl-hsk@users.noreply.github.com>
2025-03-14 20:44:46 +0100

    TASK: Change pins

--- lib/PlotbotEncoder/plotbot_parameter.h
+++ lib/PlotbotEncoder/plotbot_parameter.h
@@ -9,11 +9,11 @@ static float const meterPerClick = 6.35e-3F/20.F;
 /// On/Off switches per step that would invoke the position changed callback function?
 static byte const rotary_steps_per_click = 4;
 
-#define ROTARY_PIN_X1 16
-#define ROTARY_PIN_X2 5
-#define ROTARY_PIN_Y1 0
-#define ROTARY_PIN_Y2 2
-#define ROTARY_PIN_Z1 14
-#define ROTARY_PIN_Z2 12
+#define ROTARY_PIN_X1 D0
+#define ROTARY_PIN_X2 D1
+#define ROTARY_PIN_Y1 D2
+#define ROTARY_PIN_Y2 D5
+#define ROTARY_PIN_Z1 D6
+#define ROTARY_PIN_Z2 D7
 
 #endif
